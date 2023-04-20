7f9f54a95daefb3db43ad0c4c7742fa88063e1b9
Garrett Pace <elderpace6@gmail.com>
2023-04-20 10:27:12 -0600

    done!

--- challenge-has-unique-char/hasUniqueChars.js
+++ challenge-has-unique-char/hasUniqueChars.js
@@ -1,24 +1,36 @@
-//higher-order function
-function outer(cbf){
-    cbf()
+// //higher-order function
+// function outer(cbf){
+//     cbf()
+// }
+
+
+// function inner(){
+//     console.log("this is the inner function console.log")
+// }
+// outer(inner)//callback function
+
+
+// function inner(){
+//     console.log("this is the inner function console.log")
+// }
+// outer(function inner(){
+//     console.log("this is the inner function console.log")
+// }) // in-line callback function
+
+
+// outer(() => {
+//     console.log("this is the inner function console.log")
+// }) // arrow in-line callback function
+
+
+function hasUniqueChars(str){
+    for (let i = 0; i < str.length; i++){
+    for (let n = i+1; n < str.length; n++){
+        if(str[i] === str[n]){
+            return false
+        }
+    }
 }
-
-
-function inner(){
-    console.log("this is the inner function console.log")
+return true
 }
-outer(inner)//callback function
-
-
-function inner(){
-    console.log("this is the inner function console.log")
-}
-outer(function inner(){
-    console.log("this is the inner function console.log")
-}) // in-line callback function
-
-
-outer(() => {
-    console.log("this is the inner function console.log")
-}) // arrow in-line callback function
-
+console.log(hasUniqueChars("Bob"))
